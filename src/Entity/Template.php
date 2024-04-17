6fd27ba909876f15f68af2c5a76337d651f5b7d6
mmiralin <mmirgal868@g.educaand.es>
2024-04-17 16:05:38 +0200

    Template entity and repository created

---
+++ src/Entity/Template.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TemplateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TemplateRepository::class)]
+class Template
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $layout = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): Template
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getLayout(): ?string
+    {
+        return $this->layout;
+    }
+
+    public function setLayout(?string $layout): Template
+    {
+        $this->layout = $layout;
+        return $this;
+    }
+}
