0e73ad34ac697c3588d71919f3437280c3c648e1
mmiralin <mmirgal868@g.educaand.es>
2024-04-19 13:12:17 +0200

    User is abstract

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,7 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+abstract class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
