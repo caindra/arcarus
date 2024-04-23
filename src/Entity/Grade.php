2c5d99c7e82127c826915607de2d188921462ac5
mmiralin <mmigal868@g.educaand.es>
2024-04-23 13:11:46 +0200

    created Grade entity + repository

---
+++ src/Entity/Grade.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GradeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GradeRepository::class)]
+class Grade
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): Grade
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+}
