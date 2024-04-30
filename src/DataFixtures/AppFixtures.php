e70dc56aab17d204767e7f3e22fdbf19d7590422
mmiralin <mmigal868@g.educaand.es>
2024-04-30 13:25:06 +0200

    started creating fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AcademicYearFactory;
+use App\Factory\GroupFactory;
+use App\Factory\OrganizationFactory;
+use App\Factory\ProfessorFactory;
+use App\Factory\StudentFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +14,25 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        StudentFactory::createMany(200);
+
+        ProfessorFactory::createMany(40);
+
+        AcademicYearFactory::createMany(2);
+
+
+/*
+        GroupFactory::createMany(5, function (){
+            return [
+                'organization' => OrganizationFactory::createOne(),
+                'students' => StudentFactory::randomRange(18, 20),
+                'professors' => ProfessorFactory::randomRange(5, 7),
+                'mentors' => ProfessorFactory::randomRange(1, 2),
+                'academicYear' => AcademicYearFactory::random()
+            ];
+        });
+*/
+
 
         $manager->flush();
     }
