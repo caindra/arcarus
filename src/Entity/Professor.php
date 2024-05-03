b4dad33cebf74b71ab9d924148844bca3da86d86
mmiralin <mmirgal868@g.educaand.es>
2024-05-03 14:11:11 +0200

    assigned roles to Student & Professor + toString

--- src/Entity/Professor.php
+++ src/Entity/Professor.php
@@ -6,7 +6,14 @@ use App\Repository\ProfessorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
+/**
+ * @method string getUserIdentifier()
+ * @method string hashPassword(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+ * @method bool isPasswordValid(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+ * @method bool needsRehash(PasswordAuthenticatedUserInterface $user)
+ */
 #[ORM\Entity(repositoryClass: ProfessorRepository::class)]
 class Professor extends User
 {
@@ -24,7 +31,7 @@ class Professor extends User
         $this->groups = new ArrayCollection();
     }
 
-    public function isIsAdmin(): ?bool
+    public function getIsAdmin(): ?bool
     {
         return $this->isAdmin;
     }
@@ -75,4 +82,39 @@ class Professor extends User
     }
 
 
+    public function getRoles()
+    {
+        $roles = [];
+        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLA_PROFESSOR';
+        if ($this->getIsAdmin()) {
+            $roles[] = 'ROLE_ADMIN';
+        }
+        if ($this->getMentoredClass()) {
+            $roles[] = 'ROLE_MENTOR';
+        }
+        return array_unique($roles);
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        // TODO: Implement @method string getUserIdentifier()
+        // TODO: Implement @method string hashPassword(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+        // TODO: Implement @method bool isPasswordValid(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+        // TODO: Implement @method bool needsRehash(PasswordAuthenticatedUserInterface $user)
+    }
+
+    public function __toString(): string
+    {
+        return $this->name . ' ' . $this->surnames;
+    }
 }
--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -4,7 +4,14 @@ namespace App\Entity;
 
 use App\Repository\StudentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
+/**
+ * @method string getUserIdentifier()
+ * @method string hashPassword(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+ * @method bool isPasswordValid(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+ * @method bool needsRehash(PasswordAuthenticatedUserInterface $user)
+ */
 #[ORM\Entity(repositoryClass: StudentRepository::class)]
 class Student extends User
 {
@@ -21,5 +28,36 @@ class Student extends User
         $this->group = $group;
         return $this;
     }
+
+    public function getRoles()
+    {
+        $roles = [];
+        $roles[] = 'ROLE_USER';
+        $roles[] = 'ROLE_STUDENT';
+        return array_unique($roles);
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        // TODO: Implement @method string getUserIdentifier()
+        // TODO: Implement @method string hashPassword(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+        // TODO: Implement @method bool isPasswordValid(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+        // TODO: Implement @method bool needsRehash(PasswordAuthenticatedUserInterface $user)
+    }
+
+    public function __toString(): string
+    {
+        return $this->name . ' ' . $this->surnames;
+    }
+
 
 }
