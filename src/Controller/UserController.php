8855f061a07bc95fe4c478d4d975535021df62a5
mmiralin <mmirgal868@g.educaand.es>
2024-05-23 19:04:05 +0200

    added paginator and style to user list

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +15,19 @@ class UserController extends AbstractController
     #[Route('/users', name: 'users')]
     final public function listUsers(
         EntityManagerInterface $entityManager,
-        UserRepository $userRepository): Response
-    {
-        $users = $userRepository->findAllBySurnameName();
+        UserRepository $userRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $query = $userRepository->findAllBySurnameName();
+        $pagination = $paginator->paginate(
+            $query, // query, NOT result
+            $request->query->getInt('page', 1), // page number
+            15 // limit per page
+        );
+
         return $this->render('users/list.html.twig', [
-            'users' => $users
+            'pagination' => $pagination
         ]);
     }
 
