89414028fa622a990e2e46fb4a313b749a4b15e3
mmiralin <mmirgal868@g.educaand.es>
2024-05-26 20:14:45 +0200

    created basic files needed for dropzone and crooper

---
+++ src/Controller/CropperjsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\UxPackageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Cropperjs\Factory\CropperInterface;
+use Symfony\UX\Cropperjs\Form\CropperType;
+
+class CropperjsController extends AbstractController
+{
+
+}
