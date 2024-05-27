d761a41a5ab6998ae10ba236f0f96e824950a1ea
mmiralin <mmigal868@g.educaand.es>
2024-05-27 13:37:42 +0200

    created forms for template

---
+++ src/Form/NewTemplateType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Organization;
+use App\Entity\Template;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewTemplateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('styleName', TextType::class, [
+                'label' => 'Nombre del estilo'
+            ])
+            ->add('layout', FileType::class, [
+                'label' => 'Subir imagen',
+                'mapped' => false
+            ])
+            ->add('organization', EntityType::class, [
+                'class' => Organization::class,
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Template::class,
+        ]);
+    }
+}
