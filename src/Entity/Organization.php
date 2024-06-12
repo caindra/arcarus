bd9e0908370b3ea2e7fba25e5a187545b3a6ce4a
mmiralin <mmirgal868@g.educaand.es>
2024-06-12 16:57:44 +0200

    fixed use of asserts

--- src/Entity/Organization.php
+++ src/Entity/Organization.php
@@ -6,6 +6,7 @@ use App\Repository\OrganizationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: OrganizationRepository::class)]
 class Organization
