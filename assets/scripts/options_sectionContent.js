0ee22f9ce8b52b68b43b6e1cb62832b96322c2ed
mmiralin <mmirgal868@g.educaand.es>
2024-06-18 18:33:25 +0200

    (fix) responsive on groups

--- assets/scripts/options_sectionContent.js
+++ assets/scripts/options_sectionContent.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('input[name="has_users"]').forEach(function(elem) {
     elem.addEventListener('change', function() {
-        var userTypeOptions = document.getElementById('user-type-options');
+        let userTypeOptions = document.getElementById('user-type-options');
         if (this.value === 'yes') {
             userTypeOptions.style.display = 'block';
         } else {
